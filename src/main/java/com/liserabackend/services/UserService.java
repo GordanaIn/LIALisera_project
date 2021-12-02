a3366718aad8df5187cb298ad28a29d184ac889a
Gabriella Fry <frykhamnz@gmail.com>
2021-12-02 18:24:29 +0100

    UserService class WIP

---
+++ src/main/java/com/liserabackend/services/UserService.java
@@ -0,0 +1,17 @@
+package com.liserabackend.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    /*
+       User service for all the methods that have to do with creating the different users
+       login
+       adding role (creat student, create employee etc)
+       changing password
+     */
+
+}
