4e9dba4ee6ccb35a8db21b7f8c8b46d9d2e4c91c
Gabriella Fry <frykhamnz@gmail.com>
2021-12-07 10:45:21 +0100

    pipline (hela teamet)

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:17
-COPY ./target/liserabackend-0.0.1-SNAPSHOT.jar backend.jar
+COPY ./target/*.jar backend.jar
 ENTRYPOINT ["java","-jar","backend.jar"]
--- src/main/java/com/liserabackend/LiserabackendApplication.java
+++ src/main/java/com/liserabackend/LiserabackendApplication.java
@@ -1,6 +1,7 @@
 package com.liserabackend;
 
 import com.liserabackend.entity.*;
+import com.liserabackend.exceptions.UseException;
 import com.liserabackend.services.CompanyServiceImpl;
 import com.liserabackend.services.InternshipAdvertService;
 import com.liserabackend.services.StudentService;
@@ -26,45 +27,46 @@ public class LiserabackendApplication {
     @Bean
     CommandLineRunner run(UserService userService, StudentService studentService, CompanyServiceImpl companyService, InternshipAdvertService internshipAdvertService) {
         return args -> {
-            //add role
-            userService.saveRole(new Role("ROLE_STUDENT"));
-            userService.saveRole(new Role("ROLE_EMPLOYEE"));
-            userService.saveRole(new Role("ROLE_ADMIN"));
-            //add user
-            userService.saveUser(new User("eyuel@gmail.com", "eyuel21"));
-            userService.saveUser(new User("jafer@gmail.com", "jafer21"));
-            userService.saveUser(new User("helen@microsoft.com", "helen21"));
-            userService.saveUser(new User("josefin@google.com", "jojo21"));
-            //add role to user
-            userService.addRoleToUser("eyuel@gmail.com", "ROLE_STUDENT");
-            userService.addRoleToUser("jafer@gmail.com", "ROLE_STUDENT");
-            userService.addRoleToUser("helen@microsoft.com", "ROLE_EMPLOYEE");
-            userService.addRoleToUser("josefin@google.com", "ROLE_EMPLOYEE");
+            try {
+                //add role
+                userService.saveRole(new Role("ROLE_STUDENT"));
+                userService.saveRole(new Role("ROLE_EMPLOYEE"));
+                userService.saveRole(new Role("ROLE_ADMIN"));
+                //add user
+                userService.saveUser(new User("eyuel@gmail.com", "eyuel21"));
+                userService.saveUser(new User("jafer@gmail.com", "jafer21"));
+                userService.saveUser(new User("helen@microsoft.com", "helen21"));
+                userService.saveUser(new User("josefin@google.com", "jojo21"));
+                //add role to user
+                userService.addRoleToUser("eyuel@gmail.com", "ROLE_STUDENT");
+                userService.addRoleToUser("jafer@gmail.com", "ROLE_STUDENT");
+                userService.addRoleToUser("helen@microsoft.com", "ROLE_EMPLOYEE");
+                userService.addRoleToUser("josefin@google.com", "ROLE_EMPLOYEE");
 
-            //add student
-            Student studentEyuel = new Student("Eyuel", "Belay", "0712345611", userService.getUserByUserName("eyuel@gmail.com").get() );
-            studentEyuel.setLinkedInUrl("https://www.linkedin.com/in/eyuel-t-belay-633889167/");
-            studentEyuel.setSchoolName("EC Utbildning");
+                //add student
+                Student studentEyuel = new Student("Eyuel", "Belay", "0712345611", userService.getUserByUserName("eyuel@gmail.com").get() );
+                studentEyuel.setLinkedInUrl("https://www.linkedin.com/in/eyuel-t-belay-633889167/");
+                studentEyuel.setSchoolName("EC Utbildning");
 
-            Student studentJafer = new Student("Jafer", "Redi", "0712345667",  userService.getUserByUserName("jafer@gmail.com").get());
-            studentJafer.setLinkedInUrl("https://www.linkedin.com/in/adamjafer/");
-            studentJafer.setSchoolName("Nackademin Utbildning");
-            studentService.saveStudent(studentEyuel);
-            studentService.saveStudent(studentJafer);
+                Student studentJafer = new Student("Jafer", "Redi", "0712345667",  userService.getUserByUserName("jafer@gmail.com").get());
+                studentJafer.setLinkedInUrl("https://www.linkedin.com/in/adamjafer/");
+                studentJafer.setSchoolName("Nackademin Utbildning");
+                studentService.saveStudent(studentEyuel);
+                studentService.saveStudent(studentJafer);
 
-            Company companyMicrosoft = new Company("Microsoft", "microsoft101");
-            Company companyGoogle = new Company("Google", "google101");
-            companyService.saveCompany(companyGoogle);
-            companyService.saveCompany(companyMicrosoft);
+                Company companyMicrosoft = new Company("Microsoft", "microsoft101");
+                Company companyGoogle = new Company("Google", "google101");
+                companyService.saveCompany(companyGoogle);
+                companyService.saveCompany(companyMicrosoft);
 
 
-            Employee employeeMicrosoft = new Employee("Lisa", "Johansson", "lias@microsoft.com", userService.getUserByUserName("helen@microsoft.com").get());
-            Employee employeeGoogle = new Employee("Linda", "Eriksson", "linda@google.com", userService.getUserByUserName("josefin@google.com").get());
+                Employee employeeMicrosoft = new Employee("Lisa", "Johansson", "lias@microsoft.com", userService.getUserByUserName("helen@microsoft.com").get());
+                Employee employeeGoogle = new Employee("Linda", "Eriksson", "linda@google.com", userService.getUserByUserName("josefin@google.com").get());
 
-            employeeGoogle.setCompany(companyGoogle);
-            employeeMicrosoft.setCompany(companyMicrosoft);
-            companyService.saveEmployee(employeeMicrosoft);
-            companyService.saveEmployee(employeeGoogle);
+                employeeGoogle.setCompany(companyGoogle);
+                employeeMicrosoft.setCompany(companyMicrosoft);
+                companyService.saveEmployee(employeeMicrosoft);
+                companyService.saveEmployee(employeeGoogle);
 
            /* final var google = companyService.getCompanyByName("Google");
             final var microsoft = companyService.getCompanyByName("Microsoft");
@@ -73,17 +75,20 @@ public class LiserabackendApplication {
             companyService.saveCompany(google);
             companyService.saveCompany(microsoft);*/
 
-            InternshipAdvert internshipVacancyGoogle = new InternshipAdvert("Junior Java Developer", "Junior Java developer that has a good skill in react and springboot",
-                    "5 month duration", LocalDate.of(2021, 10, 20), "Mr Google Expert", "+745672391", 2, companyGoogle);
+                InternshipAdvert internshipVacancyGoogle = new InternshipAdvert("Junior Java Developer", "Junior Java developer that has a good skill in react and springboot",
+                        "5 month duration", LocalDate.of(2021, 10, 20), "Mr Google Expert", "+745672391", 2, companyGoogle);
 
-            InternshipAdvert internshipVacancyMicrosoft = new InternshipAdvert("Junior C# Developer", "Junior C# developer that has a good skill in react and typescript",
-                    "5 month duration", LocalDate.of(2021, 10, 21), "Mr Microsoft Expert", " 08-7525600", 1, companyMicrosoft);
+                InternshipAdvert internshipVacancyMicrosoft = new InternshipAdvert("Junior C# Developer", "Junior C# developer that has a good skill in react and typescript",
+                        "5 month duration", LocalDate.of(2021, 10, 21), "Mr Microsoft Expert", " 08-7525600", 1, companyMicrosoft);
 
-            companyGoogle.getInternshipAdvertList().add(internshipVacancyGoogle);
-            companyMicrosoft.getInternshipAdvertList().add(internshipVacancyMicrosoft);
+                companyGoogle.getInternshipAdvertList().add(internshipVacancyGoogle);
+                companyMicrosoft.getInternshipAdvertList().add(internshipVacancyMicrosoft);
 
-            internshipAdvertService.addInternshipAdvert(internshipVacancyGoogle);
-            internshipAdvertService.addInternshipAdvert(internshipVacancyMicrosoft);
+                internshipAdvertService.addInternshipAdvert(internshipVacancyGoogle);
+                internshipAdvertService.addInternshipAdvert(internshipVacancyMicrosoft);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         };
     };
 }
