943d9bebe7bbf04814c39cddedd6208c95d315c5
Debbie <deborah.solarlopez@gmail.com>
2021-12-14 16:15:22 +0100

    Adding keycloak

--- src/test/java/com/liserabackend/LiserabackendApplicationTests.java
+++ src/test/java/com/liserabackend/LiserabackendApplicationTests.java
@@ -1,13 +1,32 @@
-/*package com.liserabackend;
+package com.liserabackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class LiserabackendApplicationTests {
 
+    @Autowired
+    WebTestClient webTestClient;
+
+        /* @Autowired
+    JwtTokenProvider jwtTokenProvider;
+     */
+
     @Test
-    void contextLoads() {
+    void lialisera_user_access_success() {
+        var tokenString = ""; /* jwtTokenProvider.createToken(
+                new UsernamePasswordAuthenticationToken("test-user","Password")):*/
+
+        webTestClient
+                .get().uri("/api/student")
+                .headers(http -> http.setBearerAuth(tokenString))
+                .exchange()
+                .expectStatus().isOk();
     }
 
-}*/
+}
